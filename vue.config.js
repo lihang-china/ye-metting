0d82b8fa087761629c25e9fd4ab8fcb233a10bd7
lihang-china <864217697@qq.com>
2021-11-01 14:35:06 +0800

    gzip

--- vue.config.js
+++ vue.config.js
@@ -33,6 +33,11 @@ module.exports = {
     },
     configureWebpack: {
         plugins: [
+   new CompressionPlugin({
+                test: /\.(js|css)(\?.*)?$/i, //需要压缩的文件正则
+                threshold: 10240, //文件大小大于这个值时启用压缩
+                deleteOriginalAssets: false //压缩后保留原文件
+            }),
             new webpack.optimize.LimitChunkCountPlugin({
                 maxChunks: 4
             })
